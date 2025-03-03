47ff8dd5fd705c5fbd04db441e6675330f6d84f5
indu19-pixel <indusreeramula@gmail.com>
2025-03-04 00:53:30 +0530

    Add files via upload

---
+++ Loops/LoopDoWhile.java
@@ -0,0 +1,11 @@
+public class LoopDoWhile {
+    public static void main(String args[]){
+        
+        int n=1;
+        do{
+            System.out.print(n+" ");
+            n++;
+        }while(n<=10);
+
+    }
+}
