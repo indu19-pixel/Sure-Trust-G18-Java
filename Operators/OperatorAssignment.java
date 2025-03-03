800dce51d15dc98c68968b747866952d8e3fc250
indu19-pixel <indusreeramula@gmail.com>
2025-03-03 23:32:12 +0530

    Add files via upload

---
+++ Operators/OperatorAssignment.java
@@ -0,0 +1,24 @@
+public class OperatorAssignment {
+    public static void main(String args[]){
+        int a=10,b=5;
+
+        //Addition Assign
+        a+=b;
+        System.out.println(a);
+
+        //Subtraction Assign
+        a-=b;
+        System.out.println(a);
+
+        //Multiplication Assign
+        a*=b;
+        System.out.println(a);
+
+        //Modulus Assign
+        a%=b;
+        System.out.println(a);
+
+        //Division Assign
+        a/=b;
+        System.out.println(a);    }
+}
