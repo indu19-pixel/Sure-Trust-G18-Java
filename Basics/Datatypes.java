6e84553dbd7973d756f40fc3b2162abaa13e9ca9
indu19-pixel <indusreeramula@gmail.com>
2025-03-03 23:02:49 +0530

    Add files via upload

---
+++ Basics/Datatypes.java
@@ -0,0 +1,33 @@
+//Program on variables and Datatypes
+
+public class Datatypes {
+
+    public static void main(String args[]){
+
+        byte a=5;                                   // Range of -128 to 127 
+        System.out.println("Byte of a = "+a);
+
+        short b=50;                                // Range of -32768 to 32767
+        System.out.println("Short of b = "+b);
+
+        int c=500;                                 // Range of -2147483648 to 2147483647
+        System.out.println("Integer of c = "+c);
+
+        long d=500;                                // range of -9223372036854775808 to 9223372036854775807
+        System.out.println("Long of d = "+d);
+
+        float e=3.132f;                           // Range of -3.4e038 to 3.4e038
+        System.out.println("Float of e = "+e);
+
+        double f=55.1234334355;                   // Range of -1.7e308 to 1.7e308
+        System.out.println("Double of f = "+f);
+
+        char g='k';                               // Range of UNICODE 65535
+        System.out.println("Character of g = "+g);
+
+        boolean h=true;                           // Range of true/false
+        System.out.println("Boolean off h = "+h);
+    }
+}
+
+
