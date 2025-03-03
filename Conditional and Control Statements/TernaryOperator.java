b4dafab0bd77a07036c53f00f535160d881dbb77
indu19-pixel <indusreeramula@gmail.com>
2025-03-04 00:47:42 +0530

    Add files via upload

---
+++ Conditional and Control Statements/TernaryOperator.java
@@ -0,0 +1,9 @@
+public class TernaryOperator {
+    public static void main(String args[]){
+        //Short-hand method
+        int age=19;
+        String result=(age>=18)?"Eligible for voting":"Not Eligible";
+        System.out.println(result);
+
+    }
+}
