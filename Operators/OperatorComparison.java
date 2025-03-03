40f7738eff44d1471cc16639a06bde6183696a5d
indu19-pixel <indusreeramula@gmail.com>
2025-03-03 23:42:04 +0530

    Add files via upload

---
+++ Operators/OperatorComparison.java
@@ -0,0 +1,24 @@
+public class OperatorComparison {
+    public static void main(String args[]){
+        int a=10,b=5;
+
+        //Not equal "!"
+        if(a!=b){
+            System.out.println("a not equal to b");
+            //Greater than ">"
+            if (a>b){
+                System.out.println("a greater than b");
+            }
+            //Less than "<"
+            else if (a<b){
+                System.out.println("a less than b");
+            }
+        }
+        //Equal to "=="
+        else{
+            System.out.println("a equal to b");
+        }
+
+
+    }
+}
